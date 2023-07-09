4fd329dd6767726fa1277bd71560110242466be8
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-07-09 16:29:41 +0200

    script without horizontal scrolling

--- script.js
+++ script.js
@@ -8,52 +8,6 @@ const modalWindow = document.getElementById("modal-window");
 const closeButton = document.getElementById("close-button");
 // const div = document.querySelector(".movies-images");
 
-// Horizontal scrolling
-let activeIndexRight = 0;
-let activeIndexLeft = 0;
-let activeIndex = 0;
-
-function moveRight(i) {
-    let vignetteWidth = vignettes[i].getBoundingClientRect().width;
-    let scrollDistance = vignetteWidth * 6; // Scroll the length of 6 movies. TODO: make work for mobile because (4 movies/page instead of 6)
-
-    slider.scrollBy({
-        top: 0,
-        left: -scrollDistance,
-        behavior: "smooth",
-    });
-    activeIndexRight = (activeIndexRight - 1) % 3;
-    console.log(activeIndex);
-    updateIndicators(activeIndex);
-}
-
-function moveLeft(i) {
-    let vignetteWidth = vignettes[i].getBoundingClientRect().width;
-    let scrollDistance = vignetteWidth * 6; // Scroll the length of 6 movies. TODO: make work for mobile because (4 movies/page instead of 6)
-
-    slider.scrollBy({
-        top: 0,
-        left: -scrollDistance,
-        behavior: "smooth",
-    });
-    activeIndex = (activeIndex - 1) % 3;
-    console.log(activeIndex);
-    updateIndicators(activeIndex);
-}
-
-
-
-for (let i of [0, 1, 2, 3]) {
-    arrowsRight[i].onclick = function () {
-        moveRight(i)
-
-    };
-    arrowsLeft[i].onclick = function () {
-        moveLeft(i);
-    };
-}
-
-
 
 
 // Toggle modal window visibility
@@ -115,23 +69,10 @@ vignettes.forEach(element => {
 });
 
 
-/*
-const vignetteData = vignetteData["results"];
-if (vignetteMovie) {
-    console.log(vignetteData)
-    modalImg.setAttribute("src", img);
-    modalTitle.innerHTML = title;
-    modalGenre.innerHTML = vignetteData["genre"];
-    modalYear.innerHTML = vignetteData["year"];
-    modalDirector.innerHTML = vignetteData["director"];
-    modalActors.innerHTML = vignetteData["actors"];
-    modalImbd.innerHTML = vignetteData["avg_score"];
-    modalResume.innerHTML = vignetteData["long_description"];
-}*/
 
 
-/*
-function moveRight(i) {
+// Horizontal scrolling
+/*function moveRight(i) {
     // const originalX = cadreImg[i].getBoundingClientRect()["x"];
     const originalX = cadreImg[i].getBoundingClientRect()["x"];
     let newX = originalX;
@@ -148,81 +89,24 @@ function moveRight(i) {
 }
 function moveLeft(i) {
     // console.log(cadreImg[i].getBoundingClientRect())
-    // const originalX = cadreImg[i].getBoundingClientRect()["x"];
-    const originalX = cadreImg[i].clientWidth;
+    const originalX = cadreImg[i].getBoundingClientRect()["x"];
     let newX = originalX;
     // console.log(cadreImg[i].getBoundingClientRect())
+
     cadreImg[i].style.transition = "1.5s";
     cadreImg[i].style.transform = `translateX(-${newX + (5 * 12)}px)`;
     newX -= (5 * 16);
-    //cadreImg[i].style.transform = `translateX(-${vignettes[0].clientWidth}px)`;
-    //newX -= vignettes[0].clientWidth;
     // console.log(cadreImg[i].getBoundingClientRect())
 }
 
-function moveRight(i) {
-    const originalX = cadreImg[i].clientWidth;
-    let newX = originalX;
-    cadreImg[i].scrollBy(200, 0);
 
-}
-function moveLeft(i) {
-    const originalX = cadreImg[i].clientWidth;
-    let newX = originalX;
-    cadreImg[i].scrollTo(-200, 0);
-}
-arrowsRight[0].onclick = function () {
-    moveRight(0);
+for (let i of [0, 1, 2, 3]) {
+    arrowsRight[i].onclick = function () {
+        moveRight(i)
 
-};
+    };
+    arrowsLeft[i].onclick = function () {
+        moveLeft(i);
+    };
+}
 */
-
-/*
-// document.getElementById("img1.1").style.backgroundColor = "blue";
-// document.querySelector("h1").innerHTML = "Hello";
-// document.querySelector("h1").style.color = "blue";
-
-const bdy = document.querySelector("body");
-const btn = document.getElementsByTagName("button")[0];
-// bdy.style.backgroundColor = "blue";
-// btn.style.backgroundColor = "pink";
-arrowLeft.style.color = "red";
-
-function changeBackgroundColor() {
-    bdy.style.backgroundColor = "pink";
-}
-function revertBackgroundColor() {
-    bdy.style.backgroundColor = "black";
-}
-function random(number) {
-    return Math.floor(Math.random() * (number + 1));
-}
-
-btn.addEventListener("click", () => {
-    const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
-    bdy.style.backgroundColor = rndCol;
-});
-// arrowsRight[0].onclick = moveRight;
-// arrowsLeft[0].onclick = moveLeft;
-
-function left(value, index, array) {
-    arrowsLeft[index].onclick = revertBackgroundColor;
-}
-function right(value, index, array) {
-    array[index].onclick = changeBackgroundColor;
-}
-arrowsLeft.forEach(element => {
-    // element.onclick = revertBackgroundColor;
-    console.log(element.innerHTML)
-});
-
-
-
-function scrollLeft(arrow) {
-    const i = document.id;
-    const index = parseInt(i);
-
-}
-
-*/
-
